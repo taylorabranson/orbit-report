5d9da6902eb470193ce0ef17685aecccc5f112f2
TaylorBranson <taylor.a.branson@gmail.com>
2020-07-28 21:55:50 -0500

    step one complete

---
+++ src/app/satellite.ts
@@ -0,0 +1,15 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        name: this.name;
+        orbitType: this.orbitType;
+        type: this.type;
+        operational: this.operational;
+        launchDate: this.launchDate;
+    }
+}
