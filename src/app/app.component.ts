757dbaaec47feefee0c2d3c714fdbdc5c3880346
TaylorBranson <taylor.a.branson@gmail.com>
2020-08-02 20:58:16 -0500

    updated search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,12 +38,13 @@ export class AppComponent {
   searchTerm = searchTerm.toLowerCase();
   for(let i=0; i < this.sourceList.length; i++) {
      let name = this.sourceList[i].name.toLowerCase();
-     if (name.indexOf(searchTerm) >= 0) {
+     let type = this.sourceList[i].type.toLowerCase();
+     let orbitType= this.sourceList[i].orbitType.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0) {
         matchingSatellites.push(this.sourceList[i]);
      }
   }
-  // assign this.displayList to be the array of matching satellites
-  // this will cause Angular to re-make the table, but now only containing matches
+
   this.displayList = matchingSatellites;
 }
 
