fee52c30783319c4d1c02504443d91cae612710c
TaylorBranson <taylor.a.branson@gmail.com>
2020-08-02 20:47:43 -0500

    added satellite counts

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
-
 @Component({
   selector: 'app-orbit-counts',
   templateUrl: './orbit-counts.component.html',
@@ -9,11 +8,35 @@ import { Satellite } from '../satellite';
 })
 export class OrbitCountsComponent implements OnInit {
 
+  satelliteTypes = [
+    'Total',
+    'Space Debris',
+    'Communication', 
+    'Probe',
+    'Positioning',
+    'Space Station',
+    'Telescope'];
+
   @Input() satellites: Satellite[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  countType(satellites: Satellite[], type: string) {
+    let count: number = 0;
+
+    if (type === 'Total') {
+      return satellites.length;
+    }
+
+    for (let i = 0; i < satellites.length; i++) {
+      if (satellites[i].type === type) {
+        count += 1;
+      }
+    }
+    return count;
   }
 
 }
